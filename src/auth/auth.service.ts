77a3d02101a0dabc575128e9f6049102fde691c4
Arsalan Haq <arslan.haq@auroralolutions.io>
2023-11-02 12:12:19 +0500

    add passport

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.userService.findOne(username);
@@ -12,5 +16,13 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.name, sub: user.id };
+    console.log('payload login', payload);
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
